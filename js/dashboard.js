1b0eb6ab989e54efac0ea41de80ea97343fadb18
Avighna <avighnakc@gmail.com>
2025-04-14 17:23:50 +0530

    Added github link to CORS

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,5 @@
-// const apiUrl = 'https://avighna.pythonanywhere.com';
-const apiUrl = 'http://127.0.0.1:5000';
+const apiUrl = 'https://avighna.pythonanywhere.com';
+// const apiUrl = 'http://127.0.0.1:5000';
 
 const statuses = [
   {label: 'Unattempted', color: '', value: 0},
--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
-// const apiUrl = 'https://avighna.pythonanywhere.com';
-const apiUrl = 'http://127.0.0.1:5000';
+const apiUrl = 'https://avighna.pythonanywhere.com';
+// const apiUrl = 'http://127.0.0.1:5000';
 
 document.getElementById('login-form')
     .addEventListener('submit', async (event) => {
