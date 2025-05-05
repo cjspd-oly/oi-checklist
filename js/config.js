960919824d961007aed11cacf328d6df3c28e474
Avighna <avighnakc@gmail.com>
2025-05-05 19:53:59 +0530

    Updated api url

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // const apiUrl = 'http://127.0.0.1:5000';
 // const apiUrl = 'https://avighna.pythonanywhere.com';
-const apiUrl = 'http://avighna.xyz:5000';
+const apiUrl = 'https://avighna.xyz:8443/api/whoami';
