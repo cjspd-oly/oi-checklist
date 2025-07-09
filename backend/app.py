34ad728b5b8d57a72021cdb56b937788aab3b738
Avighna <avighnakc@gmail.com>
2025-07-09 07:56:57 +0530

    fix: Disable debug mode in production

--- backend/app.py
+++ backend/app.py
@@ -539,4 +539,4 @@ def api_logout():
     return jsonify({"success": True, "message": "JWTs are stateless, no need to logout explicitly."})
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=False, host='0.0.0.0', port=5001)
